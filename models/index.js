779e3e9c4649e926500daed0836db9e57d6bfa12
saramangelo <saramangelo@gmail.com>
2023-01-13 18:53:48 -0800

    updated through table on index.js in models

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-
-})
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
@@ -19,8 +19,14 @@ Product.belongsToMany(Tag, {
     unique: false
   },
   // define alias?
-})
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 module.exports = {
   Product,
