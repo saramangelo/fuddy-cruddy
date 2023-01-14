bdd0e17d03e2484f4e0cc1a74f9af9e724261014
saramangelo <saramangelo@gmail.com>
2023-01-13 19:45:12 -0800

    updated delete route on tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,4 +63,23 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete on tag by its `id` value
-router.delete('/:id')
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
