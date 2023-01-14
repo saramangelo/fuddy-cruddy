8a259fcf870713841ff6fcfd255d268f642a4e74
saramangelo <saramangelo@gmail.com>
2023-01-13 19:10:08 -0800

    updated PUT and DELETE routes for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,13 +49,39 @@ router.get('/', async (req, res) => {
     });
 
   // update a category by its `id` value
-router.put('/:id', (req, res) => {
-
-});
+  router.put('/:category_id', async (req, res) => {
+    const categoriesData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          category_id: req.params.category_id,
+        },
+      }
+    );
+  
+    return res.json(categoriesData);
+  });
 
   // delete a category by its `id` value
-router.delete('/:id', (req, res) => {
-
-});
+  router.delete('/:id', async (req, res) => {
+    try {
+      const categoriesData = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!categoriesData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(categoriesData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
